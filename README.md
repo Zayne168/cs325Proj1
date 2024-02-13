6b464a67c712a1714e99e84c7f26247956ddf0de
Zayne <zayne5504@hotmail.com>
2024-02-13 13:45:59 -0600

    updated all files, added requirements.yaml and README

---
+++ README.md
@@ -0,0 +1,13 @@
+#Zayne B cs325 project 1
+#
+#This project utilizes BeautifulSoup4 to scrap and strip news articles and collect there title and a brief synopsis of the article.
+#If there is no data to be gotten from the article that the URL provides, then it will flash an error saying so.
+#The code is written out and annotated in a legible and useful format for future editors.
+#This program uses Python3.9,BeautifulSoup4 and requests packages to perform its tasks.
+#To try different urls, please put the urls on there own individual line in the "urls.txt"file provided. The program reads each line as an independent url so this is important.
+#
+#
+#
+#
+#
+#
--- proj1ZB.py
+++ proj1ZB.py
@@ -1,19 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-with open('urls.txt', 'r') as file:
-    urls = file.readlines()
-for idx, url in enumerate(urls):
-    response = requests.get(url.strip())
-    soup = BeautifulSoup(response.content, 'html.parser')
+with open('urls.txt', 'r') as file:                                                     #opens urls.txt file included in folder (as variable "file")
+    urls = file.readlines()                                                             #reads each individual line into a seperate piece of an array
+for idx, url in enumerate(urls):                                                        #indexes through the urls array
+    response = requests.get(url.strip())                                                #beautiful soup scraps the url of the index
+    soup = BeautifulSoup(response.content, 'html.parser')                               #beautiful soup scraps the url of the index
 
-    try:
-        title = soup.find('title').get_text()
-        synopsis = soup.find('meta', attrs={'name': 'description'})['content']
-    except (AttributeError, KeyError):
+    try:                                                                                #checks if the url has title and description data available first
+        title = soup.find('title').get_text()                                           
+        synopsis = soup.find('meta', attrs={'name': 'description'})['content']          #if there is data on the article then it will save it in the title and synopsis("description") variables
+    except (AttributeError, KeyError):                                                  #if there isnt a data in the article it will spit an error to tell you.
         print(f"Could not find title or synopsis in {url}")
         title = "No title available"
         synopsis = "No synopsis available"
 
-    with open(f'output_{idx}.txt', 'w') as output_file:
-        output_file.write(f"Title: {title}\nSynopsis: {synopsis}")
-
+    with open(f'output_{idx}.txt', 'w') as output_file:                                 
+        output_file.write(f"Title: {title}\nSynopsis: {synopsis}")                      #index through the number of outputs to be made and writes to the file.
